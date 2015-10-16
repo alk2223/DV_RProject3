f28fe1cd2f702358ef05c19cc06ad4eb74a82ff6
Andrew Kleiman <andrewkleiman@wireless-10-146-105-187.public.utexas.edu>
2015-10-16 16:09:16 -0500

    no message

---
+++ 01 Data/R_ETL_SALARIES.R
@@ -0,0 +1,59 @@
+require(tidyr)
+require(dplyr)
+require(ggplot2)
+
+setwd("~/Desktop/CS/Data Visualization/DV_RProject3/01 Data/lahman-csv_2015-01-24 copy")
+
+file_path <- "Salaries.csv"
+
+batdf <- read.csv(file_path, stringsAsFactors = FALSE)
+
+# Replace "." (i.e., period) with "_" in the column names.
+names(batdf) <- gsub("\\.+", "_", names(batdf))
+
+measures <- c("salary", "yearID")
+
+for(n in names(batdf)) {
+  batdf[n] <- data.frame(lapply(batdf[n], gsub, pattern="[^ -~]",replacement= ""))
+}
+library(lubridate)
+dimensions <- setdiff(names(batdf), measures)
+
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    # Get rid of " and ' in dimensions.
+    batdf[d] <- data.frame(lapply(batdf[d], gsub, pattern="[\"']",replacement= ""))
+    # Change & to and in dimensions.
+    batdf[d] <- data.frame(lapply(batdf[d], gsub, pattern="&",replacement= " and "))
+    # Change : to ; in dimensions.
+    batdf[d] <- data.frame(lapply(batdf[d], gsub, pattern=":",replacement= ";"))
+  }
+}
+
+# Get rid of all characters in measures except for numbers, the - sign, and period.dimensions
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    batdf[m] <- data.frame(lapply(batdf[m], gsub, pattern="[^--.0-9]",replacement= ""))
+  }
+}
+
+batdf <- batdf %>% filter(as.numeric(as.character(yearID)) > 2000)
+
+write.csv(batdf, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
+
+Bats <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", Bats, "(") 
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+  }
+}
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+    else sql <- paste(sql, paste(m, "number(38,4)\n"))
+  }
+}
+sql <- paste(sql, ");")
+cat(sql)
+
--- 02 Data Wrangling/batting + allstar.R
+++ 02 Data Wrangling/batting + allstar.R
@@ -8,5 +8,32 @@ View(bat)
 allstar <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from ALLSTARFULL where yearID = 2010"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_alk2223', PASS='orcl_alk2223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 View(allstar)
 
-dplyr::inner_join(bat, allstar, by="PLAYERID") %>% View
+good <- dplyr::inner_join(bat, allstar, by="PLAYERID")
+View(good)
 
+
+require("jsonlite")
+require("RCurl")
+
+summary(good)
+head(good)
+
+require(extrafont)
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  #facet_wrap(~LGID.y) +
+  #facet_grid(.~SURVIVED, labeller=label_both) +
+  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='Baseball') +
+  labs(x="hits", y=paste("ab")) +
+  layer(data=good, 
+        mapping=aes(x=as.numeric(as.character(H)), y=as.numeric(as.character(AB)), color=LGID.y), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )
